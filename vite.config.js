579eee1db9fbdc458ea8d1c27c22db34fd19e699
devturakhiya2006 <dev.turakhiya2006@gmail.com>
2026-06-25 01:18:58 +0530

    Make base path dynamic

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,11 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [    tailwindcss(),react()],
-base: '/Portfolio-website/',}
-)
+export default defineConfig(({ mode }) => {
+  return {
+    plugins: [tailwindcss(), react()],
+    // If we are building for GitHub, use the subfolder, otherwise use root
+    base: mode === 'production' ? '/Portfolio-website/' : '/',
+  }
+})
